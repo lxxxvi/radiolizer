bf1b843604c9d471d6126695029ed2f4b7e545d3
Mario Schuettel <github@lxxxvi.ch>
2015-12-03 15:37:22 +0100

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Radiolizer::Application.config.secret_key_base = 'cca8179a2ea53d631001e3839b6cb45db4821002b96c0a8a81e9e504caac4cef1d90bca86a79db3a472b116e428160b11825d0acaf2e6f01b45b771e86743291'
